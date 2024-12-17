2530d3ae7bbda24cc998779eb8847dbd3e8897d4
Koala743 <armijosfeo5@gmail.com>
2024-12-17 15:47:15 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,22 @@
-
+spawn(function()
+    listaJugadores = {"Fernanflop091o", "armijosfernando2178", "elmegafer"}
+    while true do
+        for _, jugador in pairs(game.Players:GetPlayers()) do
+            local esJugadorValido = false
+            for _, nombre in pairs(listaJugadores) do
+                if jugador.Name == nombre then
+                    esJugadorValido = true
+                    break
+                end
+            end
+            if esJugadorValido then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Koala743/Koala7/refs/heads/main/Main2.lua"))()
+                print("Jugador válido: " .. jugador.Name)
+            else
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Koala743/Koala7/refs/heads/main/Main3.lua"))()
+                print("Jugador NO válido: " .. jugador.Name)
+            end
+        end
+        wait(1)
+    end
+end)
