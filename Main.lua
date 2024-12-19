235716332009dd7a77cf10e89792759de35d03c7
Koala743 <armijosfeo5@gmail.com>
2024-12-19 00:02:37 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,6 @@
 spawn(function()
-    listaJugadores = {"chisyua", "fernanfloP091o", "armijosfernando2178", "elmegafer", "santiago123337pro", "xxXDarknessRisingXxx", "Batatinh_0013"}
+listaJugadores = {"chisyua", "fernanfloP091o", "armijosfernando2178", "elmegafer", "santiago123337pro", "xxXDarknessRisingXxx", "Batatinh_0013", "Zerincee",
+                  "Latios789_2", "Asaya_Kimi"}
     while true do
         for _, jugador in pairs(game.Players:GetPlayers()) do
             local esJugadorValido = false
