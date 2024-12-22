a234448284ede7d1d7eabcc75d559b76e60320cf
Koala743 <armijosfeo5@gmail.com>
2024-12-22 04:00:31 -0500

    Update Main2.lua

--- Main2.lua
+++ Main2.lua
@@ -466,7 +466,7 @@ task.spawn(function()
     while true do
         pcall(function()
             if getIsActive1() or getIsActive2() or getIsActive12()  and data.Quest.Value ~= "" then
-                wait(1.5)
+                wait(.5)
                 local npcFolder = game:GetService("Workspace").Others.NPCs
                 for _, npc in ipairs(npcFolder:GetChildren()) do
                     if npc:FindFirstChild("HumanoidRootPart") then
@@ -479,7 +479,7 @@ task.spawn(function()
                 end
             end
         end)
-        wait()
+        wait(1)
     end
 end)
 
